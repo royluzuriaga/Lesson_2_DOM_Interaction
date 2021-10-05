5271effe6a865e98a880db8677ae05a6544d28cc
Roy Luzuriaga <luzuriagaroy@gmail.com>
2021-10-05 14:40:59 +0800

    Edit Html and Js methods

--- activity 1 - Data Binding Problem/app.js
+++ activity 1 - Data Binding Problem/app.js
@@ -1,5 +1,12 @@
-const app = Vue.createApp({    
-    
+const app = Vue.createApp({  
+      data: {
+          Name: 'Roy Luzuriaga',
+          Age: 20,
+          url: "http://vuejs.org/images/logo.png"
+
+
+      },
+
     
 
 });
