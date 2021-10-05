e5a5aed131be077f81d2bc16c95e970fc4486f6d
Roy Luzuriaga <luzuriagaroy@gmail.com>
2021-10-05 15:11:32 +0800

    activity 2 - js methods

--- activity 2 - Event Binding Problem/app.js
+++ activity 2 - Event Binding Problem/app.js
@@ -1,11 +1,22 @@
 const app = Vue.createApp({    
     data() {
-        return {        
+        return {
+            output1: '',
+            output2: ''     
            
         };        
     },
     methods: {
-        
+        showAlert() {
+            alert('Hello, This is an alert!')
+        },
+        setOutput2(event) {
+            this.output2 = event.target.value
+        },
+        setOutput1(event) {
+            this.output1 = event.target.value
+        }
+
     }
     
 });
