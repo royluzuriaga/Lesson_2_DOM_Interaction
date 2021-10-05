34fd29b6454144557b73203c89e6927df7b790e5
Roy Luzuriaga <luzuriagaroy@gmail.com>
2021-10-05 15:30:16 +0800

    activity 3 - JS methods

--- activity 3 - Reactivity Problem/app.js
+++ activity 3 - Reactivity Problem/app.js
@@ -1,9 +1,32 @@
 const app = Vue.createApp({    
     data() {
-        return {        
+        return { 
+            counter: 0,
+            value: 0,
            
         };        
     },
+    methods: {
+        add() {
+            this.counter = this.counter +5;
+        },
+        Add() {
+            this.counter = this.counter +1;
+        },
+        result() {
+            if(this.counter == 0) {
+                return this.counter;
+            }
+            else if(this.counter > 0 && this.counter < 37){
+                return'not there yet';
+            }
+            else if(this.counter > 37) {
+                return'too much';
+            }
+            return this.counter;
+        },
+    },
+    
     
 });
 app.mount('#assignment');
