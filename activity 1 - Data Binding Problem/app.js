cb949f1711b2e072ca1e73a2c246dfcf2952358e
Roy Luzuriaga <luzuriagaroy@gmail.com>
2021-10-05 14:42:23 +0800

    Remove and edit

--- activity 1 - Data Binding Problem/app.js
+++ activity 1 - Data Binding Problem/app.js
@@ -1,12 +1,26 @@
 const app = Vue.createApp({  
-      data: {
+      data () {
+          return {
           Name: 'Roy Luzuriaga',
           Age: 20,
           url: "http://vuejs.org/images/logo.png"
-
+          };
 
       },
+        methods:{
+            Five() {
+                return this.Age+5;
+            },
+            Fnumber() {
+                const randomNumber = Math.random();
+                if(randomNumber < 0.5) {
+                    return '0';
+                }else {
+                    return '1';
+                }
+            }
 
+        }
     
 
 });
