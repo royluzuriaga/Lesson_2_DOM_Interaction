7c495eb976c2d83bbd2081afb61c1291111523b2
Roy Luzuriaga <luzuriagaroy@gmail.com>
2021-10-05 16:44:57 +0800

    activity 4 -  html remove and edit, js const app

---
+++ activity 4 - Dynamic Styling Problem/app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({    
+    data() {
+        return {  
+            user: '',
+            user1: false,
+            user2: false,
+            active: true,
+            color: '',
+            keyColor: '',
+
+           
+        };        
+    },
+
+    
+});
+app.mount('#assignment');
