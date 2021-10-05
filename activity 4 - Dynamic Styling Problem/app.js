bae5be9f59743df9c7862ee91e4eb37143ab0f76
Roy Luzuriaga <luzuriagaroy@gmail.com>
2021-10-05 16:46:24 +0800

    activity 4 - methods and finish

--- activity 4 - Dynamic Styling Problem/app.js
+++ activity 4 - Dynamic Styling Problem/app.js
@@ -11,7 +11,37 @@ const app = Vue.createApp({
            
         };        
     },
+    methods: {
+        getInput(event){
+            this.user = event.target.value;
 
+            if(this.user === 'user1'){
+                this.user1 = !this.user1;
+            }else if(this.user ==='user2'){
+                this.user2 = !this.user2;
+            }else{
+                this.user1 = ''
+                this.user2 = ''
+                
+            }
+        },
+        toggleParagraph(){
+            this.active = !this.active;
+        },
+
+        PickColor(event){
+            this.color = event.target.value;
+            if(this.color === 'red'){
+                this.keyColor = this.color;
+            }else if(this.color === 'blue'){
+                this.keyColor = this.color
+            }else if(this.color === 'black'){
+                this.keyColor = this.color;
+            }else{
+                this.keyColor = ''
+            }
+        }
+    }
     
 });
 app.mount('#assignment');
